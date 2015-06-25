9166cfbbafd3c5ed3c8d2d9b4ee7e638b1803426
nasa9084 <nasa9084@live.jp>
2015-06-26 01:26:39 +0900

    Create MakeAbsentForm.sh
    Shell Script that makes Absent Form in Japanese.
    This Script needs platex and dvipdfmx commands.
    And this needs evince(PDF Viewer).
    
    Options:
    -o: Output File name.

---
+++ MakeAbsentForm.sh
@@ -0,0 +1,140 @@
+#!usr/bin/sh
+today=$(date +%Y年%m月%d日)
+
+while getopts o: OPT
+do
+    case $OPT in
+        o) fname=$OPTARG ;;
+    esac
+done
+echo "$fname"
+
+echo "名前[default:名無しの権兵衛]？"
+read name
+if [ "$name" = '' ];then
+    name='名無しの権兵衛'
+fi
+echo "学生番号[default:00000000]？"
+read id
+if [ "$id" = '' ];then
+    id='00000000'
+fi
+echo "提出日[default:" $today "]？"
+read date
+if [ "$date" = '' ];then
+    date=$today
+fi
+echo "学科[default:情報エレクトロニクス]？"
+read department
+if [ "$department" = '' ];then
+    department='情報エレクトロニクス'
+fi
+echo "コース[default:コンピュータサイエンス]？"
+read cource
+if [ "$cource" = '' ];then
+    cource='コンピュータサイエンス'
+fi
+echo "学年[default:3]？"
+read grade
+if [ "$grade" = '' ];then
+    grade=3
+fi
+echo "理由[default:体調不良]？"
+read reason
+if [ "$reason" = '' ];then
+    reason='体調不良'
+fi
+echo "欠席日[default:" $today "]？"
+read absentdate
+if [ "$absentdate" = '' ];then
+    absentdate=$today
+fi
+echo "科目[default: 算数]？"
+read subject
+if [ "$subject" = '' ];then
+    subject='算数'
+fi
+echo "教員氏名[default: Jack]？"
+read teacher
+if [ "$teacher" = '' ];then
+    teacher='Jack'
+fi
+
+echo "\documentclass[a4paper]{jarticle}
+\usepackage{absent-form}
+\usepackage[top=30truemm, bottom=30truemm, left=25truemm, right=25truemm]{geometry}
+
+\author{"$name"}
+\id{"$id"}
+\date{"$date"}
+\teacher{"$teacher"}
+\subject{"$subject"}
+\department{"$department"}
+\course{"$cource"}
+\grade{"$grade"}
+\reason{"$reason"}
+\absentdate{"$absentdate"}
+
+\begin{document}
+\absentform
+
+\end{document}" > absentform.tex
+
+echo '\def\absentform{
+\setcounter{page}{0}
+\pagestyle{empty}
+\null
+
+\begin{center}
+ {\huge {\sc 欠席届}}\\
+\end{center}
+\vskip 1cm
+\begin{flushright}
+ \@date\\
+\end{flushright}
+\vskip 1cm
+\begin{flushleft}
+ {\large \@teacher 先生\\
+ \vskip 1cm
+ 科目名: \@subject\\
+ \vskip1cm
+ 工学部 \@department 学科 \@course コース \@grade 年\\
+ 学生番号: \@id\\
+ 氏名: \@author\\}
+\end{flushleft}
+\begin{center}
+ {\large 下記の理由により欠席しましたので、お届けします。\\
+ \vskip 2cm
+ 記\\}
+\end{center}
+\vskip 2cm
+\begin{flushleft}
+ 欠席理由: \@reason\\
+ \vskip 1cm
+ 欠席日: \@absentdate\\
+\end{flushleft}
+\vfil\newpage}
+
+\def\teacher#1{\gdef\@teacher{#1}}
+\def\subject#1{\gdef\@subject{#1}}
+\def\department#1{\gdef\@department{#1}}
+\def\course#1{\gdef\@course{#1}}
+\def\grade#1{\gdef\@grade{#1}}
+\def\id#1{\gdef\@id{#1}}
+\def\reason#1{\gdef\@reason{#1}}
+\def\absentdate#1{\gdef\@absentdate{#1}}' > absent-form.sty
+
+if [ "$fname" = '' ];then
+    fname=absentform_`date +%Y_%m_%d`_"$subject".pdf
+fi
+
+platex -interaction=nonstopmode absentform.tex
+dvipdfmx -o absentform.pdf absentform.dvi
+mv absentform.pdf "$fname"
+
+for i in absent-form.sty absentform.aux absentform.dvi absentform.log absentform.tex
+do
+    rm $i
+done
+
+evince "$fname"
