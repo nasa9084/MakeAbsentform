baa20b021fc9018a51444b851ddff784d9bbe5f9
nasa9084 <nasa9084@live.jp>
2015-07-12 01:02:24 +0900

    add help

--- MakeAbsentForm.sh
+++ MakeAbsentForm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 today=$(date +%Y年%m月%d日)
 
-while getopts o:n:i:d:S:D:C:G:r:a:s:t: OPT
+while getopts o:n:i:d:S:D:C:G:r:a:s:t:h OPT
 do
     case $OPT in
         o) fname=$OPTARG ;;
@@ -16,6 +16,29 @@ do
         a) absentdate=$OPTARG ;;
         s) subject=$OPTARG ;;
         t) teacher=$OPTARG ;;
+        h) cat<<EOF
+USAGE: sh ./MakeAbsentForm.sh [OPTION ARG]...
+e.g.) sh ./MakeAbsentForm.sh -o Absentform.pdf
+
+次のオプションが使用できます。引数にはスペースを含まないでください。
+オプションを指定した場合、その項目は聞かれず、指定しなかった項目はあとから質問されます。
+(出力ファイル名のみ指定無しでデフォルト出力)
+
+-o FILENAME   出力ファイル名を指定。拡張子pdfまで記入してください。
+-n NAME       名前
+-i ID         学生番号
+-d DATE       提出日
+-S SECTION    学部
+-D DEPARTMENT 学科
+-C COURCE     コース
+-G GRADE      学年
+-r REASON     欠席理由
+-a ABSENTDATE 欠席日
+-s SUBJECT    科目名
+-t TEACHER    教員名
+-h            このヘルプを表示します。
+EOF
+           exit 0 ;;
     esac
 done
 echo "$fname"
